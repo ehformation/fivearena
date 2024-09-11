1f781305361d93c5c0a40554cc05052bcca9023e
eliewebavenue <elie@webavenue.agency>
2024-09-11 14:39:43 +0200

    add swipper js

---
+++ html/js/main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
